f8b6a6986d4d9ad575fe3563a9c081919468a965
Roman Dzieciol <rd@romans-mbp.home>
2019-03-07 08:17:15 +0000

    Hello

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `The joy of programming`,
+    author: `Roman Dzieciol`,
+    description: `The fun is in details`,
+    siteUrl: `https://roman.dev/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `roman_dzieciol`,
     },
   },
   plugins: [
